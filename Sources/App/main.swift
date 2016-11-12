85473d11f8ce113517d8940bbae567345ccec4af
Paul Pires <paulpires101@gmail.com>
2016-11-12 18:52:08 +0000

    added stub CV page

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,9 +3,11 @@ import Vapor
 let drop = Droplet()
 
 drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": "again"
-    ])
+    return try drop.view.make("home")
+}
+
+drop.get("/cv") { req in
+    return try drop.view.make("cv")
 }
 
 drop.run()
